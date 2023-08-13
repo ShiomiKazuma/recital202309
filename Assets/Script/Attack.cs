c992fdd7fd6d1a89d33fb257d58d516dd0afebdd
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2023-08-13 22:29:37 +0900

    [add]攻撃判定を一定時間後に削除する処理を追加

--- Assets/Script/Attack.cs
+++ Assets/Script/Attack.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
+    [SerializeField] float _destroyTime = 2.0f;
+    float _time = 0;
+    private void Start()
+    {
+        _time = 0;
+    }
+    private void Update()
+    {
+        _time += Time.deltaTime;
+        if (_time > _destroyTime)
+        {
+            Destroy(gameObject);
+        }
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
